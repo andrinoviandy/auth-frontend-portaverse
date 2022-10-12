352b2fba1cab7da5dfa9354c891e6bed98d84c28
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2022-10-12 15:46:10 +0700

    change logo

--- src/Components/Layouts/MainLayout.jsx
+++ src/Components/Layouts/MainLayout.jsx
@@ -1,20 +1,19 @@
 import { Outlet } from "react-router-dom";
-import PortaverseLogo from "../Assets/Pictures/PortaverseLogo.png";
 import Footer from "../Footer";
 
 export default function MainLayout() {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <div className="container mx-auto flex flex-col gap-2">
-        <nav className="p-5">
+        {/* <nav className="p-5">
           <img
             src={PortaverseLogo}
             alt="company_logo"
             className="h-[100px] p-2"
           />
-        </nav>
+        </nav> */}
 
-        <main className="scroll-smooth px-5">
+        <main className="scroll-smooth px-5 h-[calc(100vh-200px)] flex flex-col items-center justify-center pt-20">
           <Outlet />
         </main>
       </div>
