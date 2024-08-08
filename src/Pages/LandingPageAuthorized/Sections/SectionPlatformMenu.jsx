e7d9dcfc0d16089333f739655a69caa99675b74a
Muhamad Elang Ramadhan <3203012811010001@pelindoholding.onmicrosoft.com>
2024-08-08 03:35:36 +0000

    feat: show IMS & CMS menu (contd.)

--- src/Pages/LandingPageAuthorized/Sections/SectionPlatformMenu.jsx
+++ src/Pages/LandingPageAuthorized/Sections/SectionPlatformMenu.jsx
@@ -681,7 +681,7 @@ export default function SectionPlatformMenu() {
         sx={MANTINE_TAB_STYLES.default.sx}
       >
         <Tabs.List grow>
-          {["KMS", "LMS", "TMS"].map((tab) => (
+          {["KMS", "LMS", "TMS", "IMS", "CMS"].map((tab) => (
             <Tabs.Tab
               key={tab}
               sx={MANTINE_TAB_STYLES.default.sxChild}
