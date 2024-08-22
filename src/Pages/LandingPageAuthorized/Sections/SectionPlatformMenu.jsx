60920acf03cdaaf7e6e5d531c2de3521e5647196
Muhamad Elang Ramadhan KMPlus <3203012811010001@pelindoholding.onmicrosoft.com>
2024-08-22 06:42:12 +0000

    feat: show CMS menu

--- src/Pages/LandingPageAuthorized/Sections/SectionPlatformMenu.jsx
+++ src/Pages/LandingPageAuthorized/Sections/SectionPlatformMenu.jsx
@@ -674,7 +674,7 @@ export default function SectionPlatformMenu() {
         sx={MANTINE_TAB_STYLES.default.sx}
       >
         <Tabs.List grow>
-          {["KMS", "LMS", "TMS", "IMS"].map((tab) => (
+          {["KMS", "LMS", "TMS", "IMS", "CMS"].map((tab) => (
             // {["KMS", "LMS", "TMS", "IMS", "CMS"].map((tab) => (
             <Tabs.Tab
               key={tab}
