ea899d25200a46209bae16e0912f5dac0e29be06
andrinoviandy <andri.noviandy@ilcs.co.id>
2025-12-16 23:48:22 +0700

    keycloak

--- src/Networks/LoginSsoV2.js
+++ src/Networks/LoginSsoV2.js
@@ -8,59 +8,57 @@ export default async function postLoginSsoV2(payload, setIsLoading, setMessage) 
 
     const res = await axiosSSOClient.post("/auth/after-login-sso", payload);
 
-    console.log('ress', res);
+    const { idToken, targetUID } = res.data.data;
 
-    // const { idToken, targetUID } = res.data.data;
+    if (res?.data?.success === true) {
+      localStorage.setItem("accessTokenSso", payload?.access_token)
+      localStorage.setItem("refreshTokenSso", payload?.refresh_token)
+      localStorage.setItem("idTokenSso", payload?.id_token)
+      localStorage.setItem("session_id", payload?.refresh_token)
+      if (payload.isRemember) {
+        localStorage.setItem("access_token", payload?.access_token);
+      } else {
+        sessionStorage.setItem("access_token", payload?.access_token);
+      }
 
-    // if (res?.data?.success === true) {
-    //   localStorage.setItem("accessTokenSso", payload?.access_token)
-    //   localStorage.setItem("refreshTokenSso", payload?.refresh_token)
-    //   localStorage.setItem("idTokenSso", payload?.id_token)
-    //   localStorage.setItem("session_id", payload?.refresh_token)
-    //   if (payload.isRemember) {
-    //     localStorage.setItem("access_token", payload?.access_token);
-    //   } else {
-    //     sessionStorage.setItem("access_token", payload?.access_token);
-    //   }
+      const data = {
+        isRemember: false,
+        targetUID: targetUID
+      };
 
-    //   const data = {
-    //     isRemember: false,
-    //     targetUID: targetUID
-    //   };
-
-    //   // 🔹 Hit backend login langsung
-    //   setMessage("Redirect ke Dashboard Portaverse")
-    //   axiosSSOClient
-    //     .post("/auth/after-login", data, {
-    //       headers: { Authorization: `Bearer ${idToken}` },
-    //     })
-    //     .then((res) => {
-    //       setMessage("Berhasil Login Portaverse")
-    //       if (res.data.data.user.role_code.includes("SBCN")) {
-    //         window.location = `${import.meta.env.VITE_LMS_URL
-    //           }/subcon-management/${res.data.data.user?.subcon?.subcon_id
-    //           }`;
-    //         return;
-    //       }
-    //       if (res.data.data.user.role_code.includes("VNDR")) {
-    //         window.location = `${import.meta.env.VITE_LMS_URL
-    //           }/vendor-management/${res.data.data.user?.vendor?.vendor_id
-    //           }`;
-    //         return;
-    //       }
-    //       if (res.data.data.user.role_code.includes("CADH")) {
-    //         window.location = `${import.meta.env.VITE_CMS_URL
-    //           }/change-catalyst-team-monitoring-system`;
-    //         return;
-    //       }
-    //       if (res.data.data.user.role_code.includes("CADC")) {
-    //         window.location = `${import.meta.env.VITE_CMS_URL
-    //           }/change-catalyst-team-monitoring-system`;
-    //         return;
-    //       }
-    //       window.location.href = "/landing";
-    //     })
-    // }
+      // 🔹 Hit backend login langsung
+      setMessage("Redirect ke Dashboard Portaverse")
+      axiosSSOClient
+        .post("/auth/after-login", data, {
+          headers: { Authorization: `Bearer ${idToken}` },
+        })
+        .then((res) => {
+          setMessage("Berhasil Login Portaverse")
+          if (res.data.data.user.role_code.includes("SBCN")) {
+            window.location = `${import.meta.env.VITE_LMS_URL
+              }/subcon-management/${res.data.data.user?.subcon?.subcon_id
+              }`;
+            return;
+          }
+          if (res.data.data.user.role_code.includes("VNDR")) {
+            window.location = `${import.meta.env.VITE_LMS_URL
+              }/vendor-management/${res.data.data.user?.vendor?.vendor_id
+              }`;
+            return;
+          }
+          if (res.data.data.user.role_code.includes("CADH")) {
+            window.location = `${import.meta.env.VITE_CMS_URL
+              }/change-catalyst-team-monitoring-system`;
+            return;
+          }
+          if (res.data.data.user.role_code.includes("CADC")) {
+            window.location = `${import.meta.env.VITE_CMS_URL
+              }/change-catalyst-team-monitoring-system`;
+            return;
+          }
+          window.location.href = "/landing";
+        })
+    }
 
   } catch (err) {
     console.error("Login error:", err);
